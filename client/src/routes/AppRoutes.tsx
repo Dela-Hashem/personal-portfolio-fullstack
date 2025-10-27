5301b18e59955975b9fb1e6aedd9805575437818
dela-hashem <dela.hashem89@gmail.com>
2025-10-27 12:38:24 +0800

    feat(client): bootstrap React+TS app with basic routes

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/projects", element: <Projects /> },
+  { path: "/contact", element: <Contact /> },
+]);
